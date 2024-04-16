e7c36dc72cb7aa7cfa506bffb32031d821ad0940
devGustavoMorais <gustavo.morais100398@gmail.com>
2024-04-15 21:30:53 -0300

    refactor send email service

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,60 +1,16 @@
 import { CreateUser } from "#/@types/user";
-import { generateTemplate } from "#/mail/template";
-import emailVerificationToken from "#/models/emailVerificationToken";
 import User from "#/models/user";
 import { generateToken } from "#/utils/helper";
-import { MAILTRAP_PASSWORD, MAILTRAP_USER } from "#/utils/variables";
+import { sendVerificationMail } from "#/utils/mail";
 import { RequestHandler } from "express";
-import nodemailer from 'nodemailer';
-import path from 'path'
 
 export const createUser: RequestHandler = async (req: CreateUser, res) => {
     const { email, password, name } = req.body;
+
     const user = User.create({ name, email, password });
 
-
-    var transport = nodemailer.createTransport({
-        host: "sandbox.smtp.mailtrap.io",
-        port: 2525,
-        auth: {
-            user: MAILTRAP_USER,
-            pass: MAILTRAP_PASSWORD
-        }
-    });
-
     const token = generateToken()
-    const newToken = await emailVerificationToken.create({
-        owner: (await user)._id,
-        token
-    });
+    sendVerificationMail(token, { name, email, userId: (await user)._id.toString() })
 
-    const welcomeMessage = `Hi ${name}, welcome to Podify! There are so much thing that we do for verified users. Use the given OTP to verify your email.`;
-
-    transport.sendMail({
-        to: (await user)?.email,
-        from: "auth@podify.com",
-        subject: "Welcome message",
-        html: generateTemplate({
-            title: "Welcome to Podify",
-            message: welcomeMessage,
-            logo: "cid:logo",
-            banner: "cid:welcome",
-            link: "#",
-            btnTitle: token
-        }),
-        attachments: [
-            {
-                filename: "logo.png",
-                path: path.join(__dirname, "../mail/logo.png"),
-                cid: "logo"
-            },
-            {
-                filename: "welcome.png",
-                path: path.join(__dirname, "../mail/welcome.png"),
-                cid: "welcome"
-            }
-        ]
-    });
-
-    res.status(201).json(user);
+    res.status(201).json({ user: { id: (await user)._id, name, email } });
 }
---
+++ src/utils/mail.ts
@@ -0,0 +1,64 @@
+import nodemailer from 'nodemailer';
+import path from 'path'
+import { generateToken } from "#/utils/helper";
+import { MAILTRAP_PASSWORD, MAILTRAP_USER, VERIFICATION_EMAIL } from "#/utils/variables";
+import { generateTemplate } from "#/mail/template";
+import emailVerificationToken from "#/models/emailVerificationToken";
+
+const generateMailTransporter = () => {
+    const transport = nodemailer.createTransport({
+        host: "sandbox.smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+            user: MAILTRAP_USER,
+            pass: MAILTRAP_PASSWORD
+        }
+    });
+    return transport;
+}
+
+interface Profile {
+    name: string;
+    email: string;
+    userId: string;
+}
+
+export const sendVerificationMail = async (token: string, profile: Profile) => {
+    const transport = generateMailTransporter()
+
+    const { name, email, userId } = profile
+
+    await emailVerificationToken.create({
+        owner: userId,
+        token
+    });
+
+    const welcomeMessage = `Hi ${name}, welcome to Podify! There are so much thing that we do for verified users. Use the given OTP to verify your email.`;
+
+    transport.sendMail({
+        to: email,
+        from: VERIFICATION_EMAIL,
+        subject: "Welcome message",
+        html: generateTemplate({
+            title: "Welcome to Podify",
+            message: welcomeMessage,
+            logo: "cid:logo",
+            banner: "cid:welcome",
+            link: "#",
+            btnTitle: token
+        }),
+        attachments: [
+            {
+                filename: "logo.png",
+                path: path.join(__dirname, "../mail/logo.png"),
+                cid: "logo"
+            },
+            {
+                filename: "welcome.png",
+                path: path.join(__dirname, "../mail/welcome.png"),
+                cid: "welcome"
+            }
+        ]
+    });
+}
+
