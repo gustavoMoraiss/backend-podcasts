99b683bf747031b2990c35bf6c7e596a14490e75
devGustavoMorais <gustavo.morais100398@gmail.com>
2024-04-16 01:00:28 -0300

    Re-verification email

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -5,6 +5,7 @@ import { generateToken } from "#/utils/helper";
 import { sendVerificationMail } from "#/utils/mail";
 import { error } from "console";
 import { RequestHandler } from "express";
+import { isValidObjectId } from "mongoose";
 
 export const create: RequestHandler = async (req: CreateUserRequest, res) => {
     const { email, password, name } = req.body;
@@ -12,6 +13,11 @@ export const create: RequestHandler = async (req: CreateUserRequest, res) => {
     const user = User.create({ name, email, password });
 
     const token = generateToken()
+
+    await EmailVerificationToken.create({
+        owner: (await user)._id,
+        token
+    });
 
     sendVerificationMail(token, { name, email, userId: (await user)._id.toString() })
 
@@ -38,4 +44,37 @@ export const verifyEmail: RequestHandler = async (req: VerifyEmailRequest, res) 
     await EmailVerificationToken.findByIdAndDelete(verificationToken._id)
 
     res.json({ message: "Your email is verified." })
+}
+
+export const sendReverificationToken: RequestHandler = async (req, res) => {
+    const { userId } = req.body;
+
+    if (!isValidObjectId(userId)) res.status(403).json({ error: "Invalid request." })
+
+    const user = await User.findById(userId)
+
+    if (!user) res.status(403).json({ error: "Invalid request." })
+
+    await EmailVerificationToken.findOneAndDelete({
+        owner: userId
+    })
+
+    const token = generateToken()
+
+    await EmailVerificationToken.create({
+        owner: userId,
+        token
+    })
+
+     sendVerificationMail(token, {
+        name: user?.name ?? "",
+        email: user?.email ?? "",
+        userId: user?._id.toString() ?? ""
+    })
+
+    res.json({message: "Please, check your mail."})
+
+   
+
+
 }
--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,4 +1,4 @@
-import { create, verifyEmail } from "#/controllers/user";
+import { create, verifyEmail, sendReverificationToken } from "#/controllers/user";
 import { validate } from "#/middleware/validator";
 import { CreateuserSchema, EmailVerificationBody } from "#/utils/validationSchema";
 import { Router } from "express";
@@ -6,6 +6,7 @@ import { Router } from "express";
 const authRouter = Router();
 
 authRouter.post('/create', validate(CreateuserSchema), create);
-authRouter.post('/verify-email', validate(EmailVerificationBody),verifyEmail);
+authRouter.post('/verify-email', validate(EmailVerificationBody), verifyEmail);
+authRouter.post('/re-verify-email', sendReverificationToken);
 
 export default authRouter;
