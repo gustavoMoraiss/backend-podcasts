1804b0052328abb78ca1547aec18286f0d0f3969
devGustavoMorais <gustavo.morais100398@gmail.com>
2024-04-16 00:28:35 -0300

    validation token and userId

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,11 +1,11 @@
 import { create, verifyEmail } from "#/controllers/user";
 import { validate } from "#/middleware/validator";
-import { CreateuserSchema } from "#/utils/validationSchema";
+import { CreateuserSchema, EmailVerificationBody } from "#/utils/validationSchema";
 import { Router } from "express";
 
 const authRouter = Router();
 
 authRouter.post('/create', validate(CreateuserSchema), create);
-authRouter.post('/verify-email', verifyEmail);
+authRouter.post('/verify-email', validate(EmailVerificationBody),verifyEmail);
 
 export default authRouter;
--- src/utils/validationSchema.ts
+++ src/utils/validationSchema.ts
@@ -1,8 +1,19 @@
 import * as yup from 'yup'
+import { isValidObjectId } from 'mongoose';
 
 export const CreateuserSchema = yup.object().shape({
     name: yup.string().trim().required("Name is missing or blank.").min(3, "Name is too short or invalid.").max(25, "Name is too long."),
     email: yup.string().required("Email is missing or blank.").email("invalid email ID."),
     password: yup.string().required("Password is missing or blank").min(8, "Password is too short.").matches(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#\$%\^&\*])[a-zA-Z\d!@#\$%\^&\*]+$/, "Password is too simple")
+});
+
+export const EmailVerificationBody = yup.object().shape({
+    token: yup.string().trim().required("Invalid token."),
+    userId: yup.string().transform(function (value) {
+        if (this.isType(value) && isValidObjectId(value)) {
+            return value;
+        }
+        return "";
+    }).required("Invalid userId.")
 });
 
